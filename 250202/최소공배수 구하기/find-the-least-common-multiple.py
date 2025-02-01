84fbbeea70e9f8e2b36fc98163f89de559c73b61
bbandm <134187969+bbandm@users.noreply.github.com>
2025-02-02 02:21:57 +0900

    Title: 최소공배수 구하기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250202/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250202/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 # Write your code here!
 def minMul(n,m):
-    for i in range(max(n,m), n*m, 1):
+    for i in range(max(n,m), n*m + 1, 1):
         if i % n == 0 and i % m == 0:
             print(i)
             return;
