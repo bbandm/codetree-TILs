32b52458eb6cd32ad96046b5ce12b559f7fac9e9
bbandm <134187969+bbandm@users.noreply.github.com>
2025-02-02 02:10:43 +0900

    Title: 최대공약수 구하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250202/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250202/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,7 +2,9 @@ n, m = map(int, input().split())
 result = 1
 # Write your code here!
 def maxDiv(n,m):
-    result = 1
+    if min(n,m) == 1:
+        result = 1
+        return;
     for i in range(1, min(n,m), 1):
         if n % i == 0 and m % i == 0:
             result = i
